30a69c0eeed8d00b50b2d161ec07dc3dd064fdb3
noama2004 <noam.awadish@mail.huji.ac.il>
2019-02-28 21:42:54 +0200

    changes

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ class Photo:
         self.tags = tags
 
 class Slide:
-    def __init__(self, photo_ids, is_horizontal, tags):
+    def __init__(self, id, photo_ids, is_horizontal, tags):
+        self.id = id
         self.photo_ids = photo_ids
         self.is_horizontal = is_horizontal
         self.tags = tags
@@ -23,12 +24,12 @@ def create_slides_list(photos):
     while(photos):
         cur_photo = photos.pop()
         if cur_photo.is_horizontal:
-            new_slide = Slide([cur_photo.id], cur_photo.is_horizontal, cur_photo.tags)
+            new_slide = Slide(len(slides), [cur_photo.id], cur_photo.is_horizontal, cur_photo.tags)
             slides.append(new_slide)
         else:
             for second_photo in photos:
                 if not second_photo.is_horisontal:
                     new_tags = cur_photo.tags | second_photo.tags
-                    new_slide = Slide([cur_photo.id, second_photo.id], cur_photo.is_horizontal, new_tags)
+                    new_slide = Slide(len(slides), [cur_photo.id, second_photo.id], cur_photo.is_horizontal, new_tags)
                     slides.append(new_slide)
     return slides

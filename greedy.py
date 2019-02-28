051ac9224ca1908b29a458ef9fccedf205ab26ef
Ofir <t0n2b@ugrad.cs.ubc.ca>
2019-02-28 21:11:54 +0200

    scoring for all slides

--- greedy.py
+++ greedy.py
@@ -4,3 +4,14 @@ def score_slides(s1, s2):
     sub2 = s2.difference(s1)
     return min(len(inter), len(sub1), len(sub2))
 
+
+def slides_scores(slides):
+    scores = {}
+    for slide1 in slides:
+        for slide2 in slides:
+            if slide1 is slide2:
+                scores[(slide1, slide2)] = -1
+            else:
+                scores[(slide1, slide2)] = score_slides(slide1, slide2)
+    return scores
+

c165b5e1e1874eb084bce86a66c514598ff3fb82
noama2004 <noam.awadish@mail.huji.ac.il>
2019-02-28 21:25:21 +0200

    changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import parser
 class Photo:
     def __init__(self, id, tags, is_horizontal):
         self.id = id
@@ -6,21 +6,22 @@ class Photo:
         self.tags = tags
 
 def main(filename):
-    photos = Parser.parseCollection()
+    photos = parser.parseCollection(filename)
     slides = create_slides_list(photos)
     
     
 def create_slides_list(photos):
     slides = []
     passed_photos = []
-    while(photos)
+    while(photos):
         cur_photo = photos.pop()
-        if photo.is_horizontal:
-            new_slide = Slide([photo.id], photo.is_horizontal, photo.tags)
+        if cur_photo.is_horizontal:
+            new_slide = Slide([cur_photo.id], cur_photo.is_horizontal, cur_photo.tags)
+            slides.append(new_slide)
         else:
             for second_photo in photos:
-                if not photos.is_horisontal:
-                    new tags = photo.tags | second_photo.tags
-                    new_slide = Slide([photo.id, second_photo.id], photo.is_horizontal, tags)
-        slides.append(new_slide)
+                if not second_photo.is_horisontal:
+                    new_tags = cur_photo.tags | second_photo.tags
+                    new_slide = Slide([cur_photo.id, second_photo.id], cur_photo.is_horizontal, new_tags)
+                    slides.append(new_slide)
 
